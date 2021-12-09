e9e2cc656011277a21fda95adcf1723b8df442c5
Ernest Gilyazov <gilyazov@reg.ru>
2021-12-09 13:07:30 +0300

    App init Co-authored-by: Dmitriy Avdeev <ADGweb@users.noreply.github.com>

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="main">
+    <todo-list />
+  </div>
+  
+</template>
+
+<script>
+import TodoList from './components/TodoList.vue';
+
+export default {
+  name: 'App',
+  components: {
+    TodoList,
+  }
+}
+</script>
+
+<style>
+  .main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+    height: 100vh;
+  }
+</style>
