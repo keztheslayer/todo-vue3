cf77237658f043f54c29b9f890362dfdee63d2f7
Ernest Gilyazov <gilyazov@reg.ru>
2022-02-17 12:29:28 +0300

    Удалено ненужное

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="main">
     <theme-switcher />
-    <todo-list :data="data"/>
+    <todo-list/>
   </div>
   
 </template>
@@ -9,7 +9,6 @@
 <script>
 import TodoList from './components/TodoList.vue';
 import ThemeSwitcher from './components/ThemeSwitcher.vue';
-import fakeData from './utils/fakeData';
 
 export default {
   name: 'App',
@@ -18,11 +17,6 @@ export default {
     ThemeSwitcher,
   },
   setup() {
-    const data = fakeData;
-
-    return {
-      data
-    }
   }
 }
 </script>
