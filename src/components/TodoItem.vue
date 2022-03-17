abfda3588ca9b6230e3d42ecb569dafdfb1112a6
Ernest Gilyazov <gilyazov@reg.ru>
2022-03-17 12:00:17 +0300

    drag-n-drop start

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,22 +1,37 @@
 <template>
-    <div class="item">
+    <div class="item"
+        :data-item-id="id"
+        @dragstart="onDragStart"
+        @dragend="onDragEnd"
+        @drag="onDrag"
+        @mousedown="onMouseDown"
+        @dragenter="onDragEnter"
+        draggable="true"
+    >
         <check-box 
             :modelValue="done"
             @change="handleItemChange()" 
         />
-        <div 
+        <div
             class="item__title"
             :class="{'item__title_done': done}"
             contenteditable="true"
+            data-action="edit"
             @input="editItem"
             @keydown.enter.escape.prevent="editItem && $event.target.blur()"
         >
             {{ title }}
         </div>
-        <button
-            class="item__button-remove"
-            @click="removeItem"
-        />
+        <div class="item__buttons-wrapper">
+            <button
+                class="item__button-remove"
+                @click="removeItem"
+            />
+            <button
+                data-action="move"
+                class="item__button-move"
+            />
+        </div>
     </div>
 </template>
 <script>
@@ -49,11 +64,39 @@ export default {
         const editItem = (event) => {
            mutations.editItem( props.id, props.folderId, event.target.textContent );
         }
+        const onDragStart = (event) => {
+            event.target.style.opacity = 0.5;
+            console.log('Drag start', event);
+        }
+        const onDragEnd = (event) => {
+            event.target.removeAttribute('style');
+            console.log('Drag end', event);
+        }
+        const onDrag = () => {
+            //console.log('Drag', event);
+        }
+        const onDragEnter = (event) => {
+            event.target.style.background = "purple";
+            console.log('on drag enter', event);
+        }
+        const onMouseDown = (event) => {
+            if ( event.target.dataset.action === 'edit' ) {
+                return;
+            }
+            if (event.target.dataset.action !== 'move') {
+                event.preventDefault();
+            }
+        }
         return {
             items,
             handleItemChange,
             removeItem,
             editItem,
+            onDragStart,
+            onDragEnd,
+            onDrag,
+            onMouseDown,
+            onDragEnter,
         }
     }
 }
@@ -66,7 +109,8 @@ export default {
     margin: 8px 0;
 
     &:hover {
-        & .item__button-remove {
+        & .item__button-remove,
+        & .item__button-move {
             display: block;
         }
     }
@@ -82,14 +126,31 @@ export default {
         }
     }
 
+    &__buttons-wrapper {
+        display: flex;
+        align-items: center;
+        margin-left: auto;
+    }
+
+    &__button-move,
     &__button-remove {
         display: none;
-        margin-left: auto;
         width: 18px;
         height: 18px;
-        background: url('../assets/icons/trash.svg') no-repeat center/contain;
         filter: var(--icon-filter);
-        cursor: pointer;
+        cursor: pointer;  
+        &:hover {
+            opacity: 0.8;
+        }
+    }
+
+    &__button-remove {
+        background: url('../assets/icons/trash.svg') no-repeat center/contain;
+        margin-right: 10px;
+    }
+
+    &__button-move {
+        background: url('../assets/icons/move.svg') no-repeat center/contain;
     }
 }
 
